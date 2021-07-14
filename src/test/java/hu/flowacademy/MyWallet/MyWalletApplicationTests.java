065633cacef69c5e970f797c711d2e88d97589d9
Peter Szabo <szabo.peter1991@gmail.com>
2021-07-14 22:25:23 +0200

    Initial commit

---
+++ src/test/java/hu/flowacademy/MyWallet/MyWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.MyWallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
