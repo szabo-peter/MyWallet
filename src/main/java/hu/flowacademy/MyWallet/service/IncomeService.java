b950d94906eec6d450b9152dec14b689ee5fe920
Peter Szabo <szabo.peter1991@gmail.com>
2021-07-27 15:16:55 +0200

    Validate create income.

--- src/main/java/hu/flowacademy/MyWallet/service/IncomeService.java
+++ src/main/java/hu/flowacademy/MyWallet/service/IncomeService.java
@@ -1,7 +1,9 @@
 package hu.flowacademy.MyWallet.service;
 
+import hu.flowacademy.MyWallet.dto.CreateExpenseDTO;
 import hu.flowacademy.MyWallet.dto.CreateIncomeDTO;
 import hu.flowacademy.MyWallet.exception.MissingIDException;
+import hu.flowacademy.MyWallet.exception.ValidationException;
 import hu.flowacademy.MyWallet.model.Account;
 import hu.flowacademy.MyWallet.model.Income;
 import hu.flowacademy.MyWallet.model.IncomeCategory;
@@ -11,6 +13,7 @@ import hu.flowacademy.MyWallet.repository.IncomeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -31,6 +34,7 @@ public class IncomeService {
     }
 
     public Income createIncome(CreateIncomeDTO createIncomeDTO) {
+        validate(createIncomeDTO);
         Account account = accountRepository.findById(createIncomeDTO.getAccountID()).orElseThrow(() -> new MissingIDException("Didn't find account with this id."));
         accountRepository.save(account.toBuilder().balance(account.getBalance() + createIncomeDTO.getAmount()).build());
         IncomeCategory incomeCategory = incomeCategoryRepository.findById(createIncomeDTO.getIncomeCategoryID()).orElseThrow(() -> new MissingIDException("Didn't find incomeCategory with this id."));
@@ -50,5 +54,20 @@ public class IncomeService {
         List<Income> allIncomes = incomeRepository.findAll();
         log.info("Found ({}) incomes.", allIncomes.size());
         return allIncomes;
+    }
+    private void validate(CreateIncomeDTO createIncomeDTO) {
+        log.info("Validating createExpenseDTO.");
+        if(!StringUtils.hasText(createIncomeDTO.getIncomeCategoryID())){
+            throw new ValidationException("Income needs a category ID!");
+        }
+        if(!StringUtils.hasText(createIncomeDTO.getAccountID())){
+            throw new ValidationException("Income needs an account ID!");
+        }
+        if(!StringUtils.hasText(createIncomeDTO.getName())){
+            throw new ValidationException("Income needs a name!");
+        }
+        if(createIncomeDTO.getAmount()<=0){
+            throw new ValidationException("Income amount must be greater than 0!");
+        }
     }
 }
