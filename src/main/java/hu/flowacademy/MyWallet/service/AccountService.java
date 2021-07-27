24a1ca896d619d05a456898ec2b7b4800a47903f
Peter Szabo <szabo.peter1991@gmail.com>
2021-07-27 14:53:00 +0200

    Validate CreateAccount

--- src/main/java/hu/flowacademy/MyWallet/service/AccountService.java
+++ src/main/java/hu/flowacademy/MyWallet/service/AccountService.java
@@ -1,12 +1,14 @@
 package hu.flowacademy.MyWallet.service;
 
 import hu.flowacademy.MyWallet.dto.CreateAccountDTO;
+import hu.flowacademy.MyWallet.exception.ValidationException;
 import hu.flowacademy.MyWallet.model.Account;
 import hu.flowacademy.MyWallet.repository.AccountRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -23,6 +25,7 @@ public class AccountService {
     }
 
     public Account createAccount(CreateAccountDTO createAccountDTO) {
+        validate(createAccountDTO);
         Account createdAccount = accountRepository.save(Account.builder()
                 .name(createAccountDTO.getName())
                 .balance(createAccountDTO.getBalance())
@@ -36,5 +39,15 @@ public class AccountService {
         List<Account> allAccount = accountRepository.findAll();
         log.info("Found ({}) accounts.", allAccount.size());
         return allAccount;
+    }
+
+    private void validate(CreateAccountDTO createAccountDTO) {
+        log.info("Validating createAccountDTO.");
+        if(!StringUtils.hasText(createAccountDTO.getName())){
+            throw new ValidationException("Account need a name!");
+        }
+        if(createAccountDTO.getBalance()>=0){
+            throw new ValidationException("Account Balance must be greater then 0");
+        }
     }
 }
